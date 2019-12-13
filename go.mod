d6f553079d9f7c703faa0b35582b0289643d053f
tbwisk <273243055@qq.com>
2019-12-13 09:24:59 +0800

    update Intend to use web to change gorm model

--- go.mod
+++ go.mod
@@ -3,13 +3,13 @@ module github.com/xxjwxc/gormt
 go 1.13
 
 require (
+	github.com/gin-gonic/gin v1.5.0 // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/jinzhu/gorm v1.9.11
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/spf13/cobra v0.0.5
 	github.com/xxjwxc/public v0.0.0-20191107073037-ea6b812d567f
-	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/go-playground/validator.v9 v9.30.2
 	gopkg.in/yaml.v2 v2.2.7
 )
--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/xxjwxc/gormt/data/cmd"
+// import "github.com/xxjwxc/gormt/data/cmd"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	cmd.Execute()
+	// cmd.Execute()
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Main website",
+		})
+	})
+	r.POST("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Main website",
+		})
+	})
+	r.Run()
 }
